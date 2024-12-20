c8436c7fcd3b6c881faa30477a75cc880cc55523
Mat Cox <51195492+TehMuffinMoo@users.noreply.github.com>
2024-12-20 16:27:27 +0000

    Delete Example directory

--- Example/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// **
-// USED TO DEFINE PLUGIN CONFIGURATION
-// **
-return array(
-    'TestPlugin-Enabled' => true,
-    'TestPlugin-ConfigValue1' => 'Is One',
-    'TestPlugin-ConfigValue2' => 'Is Two'
-);
--- Example/cron.php
+++
@@ -1,5 +0,0 @@
-<?php
-$scheduler->call(function() {
-    echo "Hello World";
-    ob_flush();
-})->at('*/30 * * * *'); ## Every 30 minutes
