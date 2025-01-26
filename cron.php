c2b9e579e5992c4c45e4236ef32ef54940d45b97
TehMuffinMoo <mat.cox@tmmn.uk>
2025-01-26 18:30:56 +0000

    Add cronjob tracking

--- cron.php
+++ cron.php
@@ -1,37 +1,43 @@
 <?php
 $scheduler->call(function() {
-    $ibPlugin = new SecurityAssessment();
-    $reportFiles = $ibPlugin->getReportFiles();
-    $hoursBeforeExpiry = 4;
-    
-    $filesCleaned = array(
-        'filesCleaned' => [],
-        'directoriesCleaned' => []
-    );
-    
-    foreach ($reportFiles as $reportFile) {
-        $FullPath = $ibPlugin->getDir()['Files'].'/reports/'.$reportFile;
-        $fileAge = time() - filemtime($FullPath);
-        if ($fileAge > 4 * 3600) { // 4 hours in seconds
-            if (is_file($FullPath)) {
-                if (!unlink($FullPath)) {
-                    $ibPlugin->logging->writeLog("ReportCleanup","Error! Unable to delete report: ".$reportFile,"error");
+    try {
+        $ibPlugin = new SecurityAssessment();
+        $reportFiles = $ibPlugin->getReportFiles();
+        $hoursBeforeExpiry = 4;
+        
+        $filesCleaned = array(
+            'filesCleaned' => [],
+            'directoriesCleaned' => []
+        );
+        
+        foreach ($reportFiles as $reportFile) {
+            $FullPath = $ibPlugin->getDir()['Files'].'/reports/'.$reportFile;
+            $fileAge = time() - filemtime($FullPath);
+            if ($fileAge > 4 * 3600) { // 4 hours in seconds
+                if (is_file($FullPath)) {
+                    if (!unlink($FullPath)) {
+                        $ibPlugin->logging->writeLog("ReportCleanup","Error! Unable to delete report: ".$reportFile,"error");
+                    } else {
+                        $filesCleaned['filesCleaned'][] = $reportFile;
+                    }
                 } else {
-                    $filesCleaned['filesCleaned'][] = $reportFile;
-                }
-            } else {
-                if (!rmdirRecursive($FullPath)) {
-                    $ibPlugin->logging->writeLog("ReportCleanup","Error! Unable to delete directory: ".$reportFile,"error");
-                } else {
-                    $filesCleaned['directoriesCleaned'][] = $reportFile;
+                    if (!rmdirRecursive($FullPath)) {
+                        $ibPlugin->logging->writeLog("ReportCleanup","Error! Unable to delete directory: ".$reportFile,"error");
+                    } else {
+                        $filesCleaned['directoriesCleaned'][] = $reportFile;
+                    }
                 }
             }
         }
-    }
-    
-    if (isset($filesCleaned['filesCleaned']) && count($filesCleaned['filesCleaned']) > 0 || isset($filesCleaned['directoriesCleaned']) && count($filesCleaned['directoriesCleaned']) > 0) {
-        $ibPlugin->logging->writeLog("ReportCleanup","Successfully cleaned up old reports.","info",$filesCleaned);
-    } else {
-        $ibPlugin->logging->writeLog("ReportCleanup","Nothing to clean up.","info");
+        
+        if (isset($filesCleaned['filesCleaned']) && count($filesCleaned['filesCleaned']) > 0 || isset($filesCleaned['directoriesCleaned']) && count($filesCleaned['directoriesCleaned']) > 0) {
+            $ibPlugin->updateCronStatus('IB-Tools','Report Cleanup', 'success', 'Successfully cleaned up old reports');
+            $ibPlugin->logging->writeLog("ReportCleanup","Successfully cleaned up old reports.","info",$filesCleaned);
+        } else {
+            $ibPlugin->logging->writeLog("ReportCleanup","Nothing to clean up.","info");
+            $ibPlugin->updateCronStatus('IB-Tools','Report Cleanup', 'success', 'Nothing to clean up');
+        }
+    } catch (Exception $e) {
+        $ibPlugin->updateCronStatus('IB-Tools','Report Cleanup', 'error', $e->getMessage());
     }
 })->at('*/30 * * * *');
